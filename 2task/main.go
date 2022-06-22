c14f2ad2ca9645ecdea50825b561a8d47dda88a4
Glenn <vlasenkogalia.personal@gmail.com>
2022-06-22 13:12:04 +0300

    task 2 update fot open pull request

--- 2task/main.go
+++ 2task/main.go
@@ -4,23 +4,11 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
-	/*
-	"bufio"
-  	"os"
-	*/
 )
 
 func main(){
     input := "1 9 3 4 -5" //!!!
     var result string
-
-	//comment the line declaring input (!!!) and uncomment the one below to try your own inputs. Also uncomment imports and the code below
-
-	/*
-	fmt.Println("Enter numbers (separated by spaces) and press ENTER to get the max and min values")
-	in := bufio.NewReader(os.Stdin)
-	input, _ := in.ReadString('\n')
-	*/
 
 	//converting string to an array of int
 	str := strings.Fields(input)
@@ -49,3 +37,52 @@ func main(){
 	}
 
 }
+
+// uncomment the code below to try your own inputs
+
+/*
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	"bufio"
+  	"os"
+)
+
+func main(){
+    var result string
+
+	fmt.Println("Enter numbers (separated by spaces) and press ENTER to get the max and min values")
+	in := bufio.NewReader(os.Stdin)
+	input, _ := in.ReadString('\n')
+
+	//converting string to an array of int
+	str := strings.Fields(input)
+	var res []int
+    for i := 0; i < len(str); i++ {
+		intVar, _ := strconv.Atoi(string(str[i])) 
+		res = append(res, intVar)
+    }
+
+	//finding the max and min values
+	max := res[0]
+	min := res[0]
+	for i := 0; i < len(res); i++ {
+		if res[i] > max { max = res[i] }
+		if res[i] < min { min = res[i] }
+	}
+
+	//adjusting the result string
+	maxStr := strconv.Itoa(max)
+	minStr := strconv.Itoa(min)
+	if min == max {
+		fmt.Println(maxStr)
+	} else {
+		result = maxStr + " " + minStr
+		fmt.Println(result)
+	}
+
+}
+*/
